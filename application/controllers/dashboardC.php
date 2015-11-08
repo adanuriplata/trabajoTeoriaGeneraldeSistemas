9985c84ace063e0f573fcd8c73d07618f24fcbbb
Adan Uri Plata <adanuriplata@gmail.com>
2015-11-07 18:23:02 -0600

    se agrega login y dashboard en controlador se estructuran carpetas

---
+++ application/controllers/dashboardC.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class DashboardC extends CI_Controller {
+	// function __construct(){
+	// 	parent::__construct();
+	// }
+	public function index()
+	{
+		$this->load->view('dashboard/dashboardhead');
+		$this->load->view('dashboard/dashboardheader');
+		$this->load->view('dashboard/dashboardnav');
+		$this->load->view('dashboard/dashboardfooter');
+	}
+}
